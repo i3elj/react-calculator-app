c5b1f8dac6fe1f4c283189b20ef5c4e861151d66
i3elj <nardoguisande@gmail.com>
2022-01-25 21:41:57 -0300

    new functions

--- src/controllers/inputHandler.js
+++ src/controllers/inputHandler.js
@@ -5,15 +5,33 @@ function getValue(val) {
 }
 
 function cleanInput() {
-    document.getElementById('header-input').value = "";
+    document.getElementById("header-input").value = "";
+}
+
+function putInHistory(inp, res) {
+    let div = document.createElement("div");
+    div.className = "expression";
+    let expressionBtn = document.createElement("button");
+    let expressionTxt = document.createTextNode(inp);
+    expressionBtn.appendChild(expressionTxt);
+    let resultTag = document.createElement("p");
+    let resultTxt = document.createTextNode(res);
+    resultTag.appendChild(resultTxt);
+    div.appendChild(expressionBtn);
+    div.appendChild(resultTag);
+    document.getElementById("history").appendChild(div);
 }
 
 function evaluateExpression() {
-   document.getElementById('header-input').value = math.evaluate(document.getElementById('header-input').value);
+    let input = document.getElementById("header-input").value;
+    let result = math.evaluate(input);
+
+    document.getElementById("header-input").value = result;
+    putInHistory(input, result);
 }
 
 module.exports = {
     getValue,
     cleanInput,
-    evaluateExpression
+    evaluateExpression,
 };
