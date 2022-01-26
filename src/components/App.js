6512248066e1a0499161bf648ac67bb0e6774ea6
i3elj <nardoguisande@gmail.com>
2022-01-25 21:40:30 -0300

    added History component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import NumPad from "./NumPad";
 import Header from "./Header";
+import History from "./History";
 import "../style/App.css";
 
 const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <Header />
-                    <NumPad />
-                </div>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <History />
+        <Header />
+        <NumPad />
+      </header>
+    </div>
+  );
 };
 
 export default App;
