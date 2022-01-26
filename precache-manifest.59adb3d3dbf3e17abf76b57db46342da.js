805cf56eb6b91cf714bba313c724780026f00520
i3elj <nardoguisande@gmail.com>
2022-01-25 21:56:52 -0300

    Updates

---
+++ precache-manifest.59adb3d3dbf3e17abf76b57db46342da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9920e6e13835a03a7ee8aed5f67d5da7",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "80fae8ce0aa1168b75cf",
+    "url": "/react-calculator-app/static/css/main.841926eb.chunk.css"
+  },
+  {
+    "revision": "c6a16066f5cb6d4a009a",
+    "url": "/react-calculator-app/static/js/2.f85e57fd.chunk.js"
+  },
+  {
+    "revision": "80fae8ce0aa1168b75cf",
+    "url": "/react-calculator-app/static/js/main.15197d9b.chunk.js"
+  },
+  {
+    "revision": "75c300b67db2a3f022b0",
+    "url": "/react-calculator-app/static/js/runtime~main.81ce0e12.js"
+  }
+]);
