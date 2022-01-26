d65c2f27914642cbb1875925f2ffa2cf644fd927
i3elj <nardoguisande@gmail.com>
2022-01-25 21:40:11 -0300

    nothing changed

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./style/index.css";
 import App from "./components/App";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
